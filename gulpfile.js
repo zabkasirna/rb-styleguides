3fde149daf0cc24b66cba548e7cb6b0be623c46c
Zabka Sirna Gwen <zabka.sirna.gwen@gmail.com>
2015-05-09 15:18:52 +0700

    Gulp tasks initialized.

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+/** ------------------------------------------------------------------------- *\
+ *  START HERE!
+ ** ------------------------------------------------------------------------- */
+
+var gulp     = require('gulp')
+,   sequence = require('run-sequence')
+,   tasks    = require('./task')
+;
+
+gulp.task('default', function(done) {
+    sequence(
+        ['markups'],
+        ['styles'],
+        ['script'],
+        ['watch'],
+        done
+    );
+});
