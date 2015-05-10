ac02a28c4f3878437644bb0e7f9835ba9aeb0fa9
Zabka Sirna Gwen <zabka.sirna.gwen@gmail.com>
2015-05-11 05:02:20 +0700

    Styleguide Half-Done.

--- src/script/rb-styleguide.js
+++ src/script/rb-styleguide.js
@@ -18,6 +18,35 @@
     
     $( function() {
 
+        // convert rgb to hex value string
+        function rgb2hex(rgb) {
+            if (/^#[0-9A-F]{6}$/i.test(rgb)) { return rgb; }
+
+            rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+
+            if (rgb === null) { return "N/A"; }
+
+            function hex(x) {
+                return ("0" + parseInt(x).toString(16)).slice(-2);
+            }
+
+            return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
+        }
+
+        $('.dynamic-color .col').each(function () {
+            $(this).children().each(function () {
+                
+                var color = $(this).css('background-color'),
+                classes = $(this).attr('class');
+                
+                $(this).html(rgb2hex(color) + " " + classes);
+
+                if (classes.indexOf("darken") >= 0 || $(this).hasClass('black')) {
+                    $(this).css('color', 'rgba(255,255,255,.9');
+                }
+            });
+        });
+
         // Plugin initialization
         $('.slider').slider({full_width: true});
         $('.parallax').parallax();
